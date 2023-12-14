0303fc1181704584c45dad08ed31c1d6ff15a3f9
ELhamzaouiWalid <walidelhamzoui@gmail.com>
2023-12-14 20:06:35 +0100

    create_fuction

--- javascript.js
+++ javascript.js
@@ -1,35 +1,121 @@
 let divE = document.querySelectorAll('form .page');
+
 let button=document.querySelectorAll('button');
-let cirle=document.getElementById('cirle');
-// let nom=document.getElementById('nom');
-// let prenom=document.getElementById('prenom');
-// let email=document.getElementById('E-mail');
-// let Téléphone=document.getElementById('Téléphone');
-// let entreprise=document.getElementById('entreprise');
+let range=document.getElementById('range');
+let nom=document.getElementById('nom');
+let prenom=document.getElementById('prenom');
+let prenom_error=document.getElementById('prenom_error');
+let nom_error=document.getElementById('nom_error');
+let email_error=document.getElementById('email_error');
+let Téléphone_error=document.getElementById('telephone_error');
+let entreprise_error=document.getElementById('entreprise_error');
+let email=document.getElementById('E-mail');
+let Téléphone=document.getElementById('Téléphone');
+let entreprise=document.getElementById('entreprise');
 let btnS=document.getElementById('Suivant');
+let btnP=document.getElementById('Précédent');
+let numerique=document.getElementById('numerique');
+
 // Parse the current width as a number
 
+           
+btnP.style.display="none"
+   
+    // console.log("oki");
+    
+    
 let index=0;
 button.forEach((btn)=>{
 
 btn.addEventListener('click',(e)=>{
     e.preventDefault();
     
-    let currentWidth = parseFloat(cirle.style.width) || 0; 
-if(btn.id === "Suivant"){
-    index++
+    let currentWidth = parseFloat(range.style.width) || 0;
     
-    cirle.style.width = (currentWidth + 25) + "%"; 
+if(btn.id === "Suivant"){
+   
+    index++
+    btnP.style.display="block"
+    if((index + 1)==2){
+        if(prenom.value===""){
+        prenom_error.innerHTML="enter votre prenom"
+        prenom_error.style.color="red";
+        btnS.style.display="none"
+        btnP.style.display="block"
+       
+        // console.log("oki");
+        
+        }else{
+            btnS.style.display="block"
+        }
+        if(nom.value===""){
+        nom_error.innerHTML="enter votre nom"
+        nom_error.style.color="red";
+        btnS.style.display="none"
+       
+        // console.log("oki");
+        
+        }
+        if(email.value===""){
+        email_error.innerHTML="enter votre email"
+        email_error.style.color="red";
+        btnS.style.display="none"
+       
+        // console.log("oki");
+        
+        }
+        if (Téléphone.value === "" || !/^(\([0-9]{3}\)|[0-9]{3}-)[0-9]{3}-[0-9]{4}$/) {
+            Téléphone_error.innerHTML = "Entrez un numéro valide";
+            Téléphone_error.style.color = "red";
+            btnS.style.display = "none";
+        }
+        if(entreprise.value===""){
+            entreprise_error.innerHTML="enter votre entrese"
+            entreprise_error.style.color="red";
+        btnS.style.display="none"
+       
+        // console.log("oki");
+        
+        }
+        
+      
+        
+    }
+   
+    range.style.width = (currentWidth + 25) + "%"; 
+    
 }else{
+    if((index+1)==3){
+    // btnS.style.display="none"
+  
+    
+    }else
+    {
+        btnS.style.display="block"
+        // btnP.style.display="block"
+        
+    }
+    
 index--
-cirle.style.width = (currentWidth - 25) + "%"; 
+range.style.width = (currentWidth - 25) + "%"; 
+
 }
 
 if (index > divE.length - 1) {
     index = 0;
-    cirle.style.width = "25%";
+    range.style.width = "25%";
     // Check if input fields are empty to disable the button
    
+}
+if (divE.length ) {
+    // Parcours chaque div et affiche leur longueur
+    divE.forEach(function(div, index) {
+        console.log("Longueur de la div " + (index + 1) + " : " + div.textContent.length);
+       
+    });
+    if(index+1==1){
+ btnP.style.display="none";
+    }
 }
 
 if (index < 0) {
@@ -55,6 +141,28 @@ console.log(divE)
 
 
 });
+function verifier() {
+ 
+}
+
+function handele(){
+
+if(prenom.value!=="" && nom.value!==""&& email.value!==""&& Téléphone.value!==""&& entreprise.value!==""){
+    btnS.style.display="block";
+    prenom_error.innerHTML=""
+    nom_error.innerHTML=""
+    email_error.innerHTML=""
+    Téléphone_error.innerHTML=""
+    entreprise_error.innerHTML=""
+
+}else{
+    btnS.style.display="none"
+
+}
+
+
+}
+
 
 
 
